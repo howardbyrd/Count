939faa3e317e09b2aec507ee0b2306c3f37f395b
Howard Byrd <mhscsp16.byrd@gmail.com>
2021-05-09 23:23:16 -0400

    Changed a few words in the description.

--- count.py
+++ count.py
@@ -5,7 +5,7 @@ message = '''Practice Projects
 For practice, write programs to do the following tasks.
 
 Fantasy Game Inventory (what a name)
-You are creating a fantasy video game. The data structure to model the player’s inventory will be a dictionary where the keys are string values describing the item in the inventory and the value is an integer value detailing how many of that item the player has. For example, the dictionary value {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12} means the player has 1 rope, 6 torches, 42 gold coins, and so on.
+You are creating a fantasy video game. The data structure to model the player’s bag will be a dictionary where the keys are string values describing the item in the bag and the value is an integer value detailing how many of that item the player has. For example, the dictionary value {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12} means the player has 1 rope, 6 torches, 42 gold coins, and so on.
 
 Write a function named displayInventory() that would take any possible “inventory” and display it like the following:
 
