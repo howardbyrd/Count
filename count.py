1bef84669390620ff4af28a9309c22cef0fc14dc
Howard Byrd <mhscsp16.byrd@gmail.com>
2021-05-09 23:20:46 -0400

    Changed dragon into monster.

--- count.py
+++ count.py
@@ -33,19 +33,19 @@ def displayBag(bag):
 
 displayBag(stuff)
 List to Dictionary Function for Fantasy Game Inventory
-Imagine that a vanquished dragon’s loot is represented as a list of strings like this:
+Imagine that a vanquished monster’s loot is represented as a list of strings like this:
 
 
-dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
-Write a function named addToInventory(inventory, addedItems), where the inventory parameter is a dictionary representing the player’s inventory (like in the previous project) and the addedItems parameter is a list like dragonLoot. The addToInventory() function should return a dictionary that represents the updated inventory. Note that the addedItems list can contain multiples of the same item. Your code could look something like this:
+monsterLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+Write a function named addToInventory(inventory, addedItems), where the inventory parameter is a dictionary representing the player’s inventory (like in the previous project) and the addedItems parameter is a list like monsterLoot. The addToInventory() function should return a dictionary that represents the updated inventory. Note that the addedItems list can contain multiples of the same item. Your code could look something like this:
 
 
 def addToBag(bag, addedItems):
     # your code goes here
 
 inv = {'gold coin': 42, 'rope': 1}
-dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
-inv = addToInventory(inv, dragonLoot)
+monsterLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inv = addToInventory(inv, monsterLoot)
 displayInventory(inv)
 The previous program (with your displayInventory() function from the previous project) would output the following:
 
