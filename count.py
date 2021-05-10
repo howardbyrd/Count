09c22d3af89a0ba70f9bb8cfa366aefd3fa8e728
Howard Byrd <mhscsp16.byrd@gmail.com>
2021-05-09 23:13:17 -0400

    Changed inventory to bag.

--- count.py
+++ count.py
@@ -10,7 +10,7 @@ You are creating a fantasy video game. The data structure to model the player’
 Write a function named displayInventory() that would take any possible “inventory” and display it like the following:
 
 
-Inventory:
+Bag:
 13 arrow
 42 gold coin
 1 rope
@@ -21,17 +21,17 @@ Total number of items: 66
 Hint: You can use a for loop to loop through all the keys in a dictionary.
 
 
-# inventory.py
+# bag.py
 stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 5, 'arrow': 13}
 
-def displayInventory(inventory):
-    print("Inventory:")
+def displayBag(bag):
+    print("Bag:")
     item_total = 0
     for k, v in inventory.items():
         # FILL IN THE CODE HERE
     print("Total number of items: " + str(item_total))
 
-displayInventory(stuff)
+displayBag(stuff)
 List to Dictionary Function for Fantasy Game Inventory
 Imagine that a vanquished dragon’s loot is represented as a list of strings like this:
 
@@ -40,7 +40,7 @@ dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
 Write a function named addToInventory(inventory, addedItems), where the inventory parameter is a dictionary representing the player’s inventory (like in the previous project) and the addedItems parameter is a list like dragonLoot. The addToInventory() function should return a dictionary that represents the updated inventory. Note that the addedItems list can contain multiples of the same item. Your code could look something like this:
 
 
-def addToInventory(inventory, addedItems):
+def addToBag(bag, addedItems):
     # your code goes here
 
 inv = {'gold coin': 42, 'rope': 1}
@@ -50,7 +50,7 @@ displayInventory(inv)
 The previous program (with your displayInventory() function from the previous project) would output the following:
 
 
-Inventory:
+Bag:
 45 gold coin
 1 rope
 5 ruby
